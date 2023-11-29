ea67fb0232aba63dcccc5987b9810274eb2b1d05
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 18:06:00 +0200

    New added creating

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,6 +1,8 @@
-const User = require('../model/user.model');
+const User = require('../model/user.model.js');
 const bycrypt = require('bcrypt');
 const errorhandler = require('../Utils/error');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const signup = async (req, res,  next) => {
   const { username, email, password } = req.body;
@@ -26,4 +28,24 @@ const getUsers = async (req, res, next) => {
   }
 }
 
-module.exports = { signup , getUsers }; 
+// Adding a user singing 
+const singin =  async(req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email })
+    if (!validUser) {
+      throw new errorhandler(404, 'User not found in the system')
+    }  
+    const validPassword = bycrypt.compareSync(password, validUser.password)
+    if (!validPassword) {
+      throw new errorhandler(401, 'Wrong credentials or password')
+    } 
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '4d' })
+    const {password: pass, ...rest} = validUser._doc;
+    res.cookie('access_token', token, { httpOnly: true}).status(200).json((rest))
+  } catch (error) {
+    next(error) 
+  } 
+}
+
+module.exports = { signup , getUsers , singin}; 
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 // auth.router.js
 const express = require('express');
-const { signup, getUsers } = require('../controller/auth.controller.js');
+const { signup, getUsers, singin } = require('../controller/auth.controller.js');
 const router = express.Router();
 
 router.post('/signup', signup);
-router.get('/getUsers', getUsers);
+router.post('/singin', singin)
+router.get('/', getUsers);
 
 module.exports = router;
