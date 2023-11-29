7d2b4374d0ed6e36b22b61b4acbad2f6eba5951a
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 12:19:34 +0200

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.URL_PORT; // Use process.env.PORT or a default port like 4000
 
 
 // Creating an app listening and directing to mongoDB
@@ -11,7 +10,7 @@ const urimongoDB = process.env.ATLAS_URI;
 mongoose
     .connect(urimongoDB)
     .then(() => {
-        app.listen(port, () => {
-            console.log(`Congratulations! Now you are live on MongoDB service at port: ${port}`);
+        app.listen(4000, () => {
+            console.log("Congratulations! Now you are live on MongoDB service at port :", 4000);
           });
     })
