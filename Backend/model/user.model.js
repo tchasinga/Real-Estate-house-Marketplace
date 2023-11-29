93de3b87ee19e6f5a07aeb5d7d96e18014745cba
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 12:36:45 +0200

    new commit

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true, // This is to make sure that no two users have the same email
-        trim: true, // This is to make sure that the email is trimmed
+        trim: true, // This is to make sure that the email is triming
     },
     password: {
         type: String,
