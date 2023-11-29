6a0f06b1ac4475324cf579c287706a660b5a526b
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 14:46:01 +0200

    new code

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,11 +1,18 @@
 const User = require('../model/user.model');
+const bycrypt = require('bcrypt');
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({username, email, password}); 
-   await newUser.save()
-    .then(() => res.json('User added Successfully!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+      // Hashing the password
+      const hashedPassword = bycrypt.hashSync(password, 10);
+      // Creating a new user
+  const newUser = new User({username, email, password : hashedPassword});
+  try {
+    await newUser.save()
+    res.json('User added Successfully!')
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 }
 
 module.exports = { signup }; 
