0ee1f263905ebd1d2346774d6e9f89a4f25b1cb9
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 16:04:41 +0200

    man commit

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import '../index.css';
 import {Link} from 'react-router-dom';
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({})
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+
   return (
     <div className='p-3  max-w-lg mx-auto'>
         <h1 className="text-3xl text-center font-semibold m-7">Sing-Up</h1>
         <form action="" className='flex flex-col gap-3'>
-          <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' />
-          <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
-          <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
-          <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-60 disabled:opacity-75 p-3 font-medium'>Sing-up</button>
+          <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+          <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+          <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+          <button onSubmit={handlerSubmit} className='bg-gray-900 text-white rounded-lg uppercase hover:opacity-90  p-3 font-medium w-44'>Sing-up</button>
         </form>
         <div className='flex gap-2 mt-3 text-sm'>
           <p>Do you have an acoount?</p>
-          <Link to='/login' className='text-blue-700 hover:opacity-60'>Login now</Link>
+          <Link to='/sing-in' className='text-blue-700 hover:opacity-60'>Login now</Link>
         </div>
     </div>
   )
