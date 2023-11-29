d2e2701ad54caa5780ca6c8b1deebdb663fd8541
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-29 12:04:35 +0200

    Adding new file

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.URL_PORT; // Use process.env.PORT or a default port like 4000
 
-// Creating an app listening
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+
+// Creating an app listening and directing to mongoDB
+const urimongoDB = process.env.ATLAS_URI;
+mongoose
+    .connect(urimongoDB)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Congratulations! Now you are live on MongoDB service at port: ${port}`);
+          });
+    })
+
+
